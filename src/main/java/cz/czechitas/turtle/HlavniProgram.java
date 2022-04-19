b4883ae03ab2ed149ac5476d10d7e73a657ab65a
Filip Jirsák <filip@jirsak.org>
2022-04-19 22:52:32 +0200

    Initial commit

---
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -0,0 +1,13 @@
+package cz.czechitas.turtle;
+
+import cz.czechitas.turtle.engine.*;
+
+public class HlavniProgram {
+
+    public void main(String[] args) {
+        Turtle zofka;
+
+        zofka = new Turtle();
+    }
+
+}
---
+++ src/main/java/cz/czechitas/turtle/engine/SpousteciTrida.java
@@ -0,0 +1,13 @@
+package cz.czechitas.turtle.engine;
+
+import cz.czechitas.turtle.*;
+
+public class SpousteciTrida {
+
+    public static void main(String[] args) {
+        HlavniProgram prg;
+        prg = new HlavniProgram();
+        prg.main(args);
+    }
+
+}
