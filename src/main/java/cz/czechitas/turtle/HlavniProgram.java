00af61ee7e88ce7c4f3cfed36c8dd3929db38898
Filip Jirsák <filip@jirsak.org>
2023-02-19 22:21:05 +0100

    Aktualizace Java 17 a Gradle 8.0

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -3,11 +3,14 @@ package cz.czechitas.turtle;
 import cz.czechitas.turtle.engine.*;
 
 public class HlavniProgram {
+    private Turtle zofka;
 
-    public void main(String[] args) {
-        Turtle zofka;
-
+    public void start() {
         zofka = new Turtle();
+    }
+
+    public static void main(String[] args) {
+        new HlavniProgram().start();
     }
 
 }
